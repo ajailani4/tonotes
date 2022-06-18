7efcc673ba059a2a53f9107d2ae688e602ff9902
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-06-18 08:32:38 +0700

    Downgrade Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.android.application") version "7.2.0" apply false
     id("com.android.library") version "7.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.5.31" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.6.21" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
 }
 
 tasks.register("clean", Delete::class) {
