9553cee51073c4c46107ade72bcca7f273bfbcf1
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-06-19 22:40:12 +0700

    Update Room configuration

--- note/note_data/src/main/java/com/tonotes/note_data/local/NoteDatabase.kt
+++ note/note_data/src/main/java/com/tonotes/note_data/local/NoteDatabase.kt
@@ -1,11 +1,12 @@
 package com.tonotes.note_data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.tonotes.note_data.model.Note
 
 @Database(
     entities = [Note::class],
     version = 1
 )
-abstract class NoteDatabase {
+abstract class NoteDatabase : RoomDatabase() {
 }
--- note/note_data/src/main/java/com/tonotes/note_data/model/Note.kt
+++ note/note_data/src/main/java/com/tonotes/note_data/model/Note.kt
@@ -1,5 +1,10 @@
 package com.tonotes.note_data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Note(
+    @PrimaryKey
     val id: Int
 )
