e21de9821a1eecca8f15bed9de8c14f2a6e9b744
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-06-20 22:38:23 +0700

    Update design system (color, typography)

---
+++ app/src/main/java/com/tonotes/app/ui/theme/Font.kt
@@ -0,0 +1,10 @@
+package com.tonotes.app.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.tonotes.app.R
+
+val LatoFamily = FontFamily(
+    Font(R.font.lato_regular, FontWeight.Normal)
+)
