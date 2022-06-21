ded713c41c9b26f8f45c84b2e16ba90b099b3594
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-06-21 21:56:54 +0700

    Add SearchTextField component

--- note/note_ui/src/main/java/com/tonotes/note_ui/home/HomeScreen.kt
+++ note/note_ui/src/main/java/com/tonotes/note_ui/home/HomeScreen.kt
@@ -1,9 +1,74 @@
 package com.tonotes.note_ui.home
 
+import com.tonotes.core.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen() {
-    Text(text = "This is Home screen")
+    val (text, setText) = remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        SearchTextField(text, setText)
+    }
+}
+
+@Composable
+fun SearchTextField(
+    text: String,
+    onTextChanged: (String) -> Unit
+) {
+    var isFocused by remember { mutableStateOf(false) }
+
+    BasicTextField(
+        modifier = Modifier
+            .padding(20.dp)
+            .background(
+                color = MaterialTheme.colorScheme.surfaceVariant,
+                shape = CircleShape
+            )
+            .fillMaxWidth()
+            .onFocusChanged { isFocused = it.isFocused },
+        value = text,
+        onValueChange = onTextChanged,
+        singleLine = true,
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+        textStyle = MaterialTheme.typography.bodyLarge,
+        decorationBox = { innerTextField ->
+            Row(
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                    contentDescription = "Search icon"
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+
+                if (text.isEmpty() && !isFocused) {
+                    Text(
+                        text = stringResource(id = R.string.search_notes),
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+
+                innerTextField()
+            }
+        }
+    )
 }
