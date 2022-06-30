c7b971f512ddd3a11329a88c9d18bdc51fa8b2c9
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-06-30 10:01:30 +0700

    Add fake repository for NoteRepository

---
+++ note/note_domain/src/test/java/com/tonotes/note_domain/repository/NoteRepositoryFake.kt
@@ -0,0 +1,43 @@
+package com.tonotes.note_domain.repository
+
+import com.tonotes.core.Resource
+import com.tonotes.note_data.local.entity.NoteEntity
+import com.tonotes.note_data.repository.NoteRepository
+import com.tonotes.note_domain.util.ResourceType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class NoteRepositoryFake : NoteRepository {
+    private val notes = mutableListOf<NoteEntity>()
+    private lateinit var resourceType: ResourceType
+
+    override fun getNotes(searchQuery: String): Flow<Resource<List<NoteEntity>>> =
+        when (resourceType) {
+            ResourceType.SUCCESS -> flowOf(Resource.Success(notes))
+
+            ResourceType.ERROR -> flowOf(Resource.Error("Failed to get notes"))
+        }
+
+    override fun getNoteDetail(id: Int): Flow<Resource<NoteEntity>> =
+        when (resourceType) {
+            ResourceType.SUCCESS -> flowOf(Resource.Success(notes.find { it.id == id }))
+
+            ResourceType.ERROR -> flowOf(Resource.Error("Failed to get note detail"))
+        }
+
+    override suspend fun insertNote(noteEntity: NoteEntity) {
+        notes.add(noteEntity)
+    }
+
+    override suspend fun editNote(noteEntity: NoteEntity) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNote(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun setResourceType(type: ResourceType) {
+        resourceType = type
+    }
+}
