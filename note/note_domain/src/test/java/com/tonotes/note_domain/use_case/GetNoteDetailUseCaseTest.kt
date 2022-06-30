f38bef3fb0ea7373ec91ac354cabee034bbe07dc
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-06-30 13:42:45 +0700

    Update GetNoteDetailUseCaseTest

--- note/note_domain/src/test/java/com/tonotes/note_domain/use_case/GetNoteDetailUseCaseTest.kt
+++ note/note_domain/src/test/java/com/tonotes/note_domain/use_case/GetNoteDetailUseCaseTest.kt
@@ -1,8 +1,10 @@
 package com.tonotes.note_domain.use_case
 
 import com.tonotes.core.Resource
+import com.tonotes.note_domain.model.Note
 import com.tonotes.note_domain.repository.NoteRepositoryFake
 import com.tonotes.note_domain.util.ResourceType
+import com.tonotes.note_domain.util.note
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
@@ -24,23 +26,25 @@ class GetNoteDetailUseCaseTest {
     fun `Get note detail should return success`() = runBlocking {
         noteRepositoryFake.setResourceType(ResourceType.SUCCESS)
 
-        val isSuccess = when (getNoteDetailUseCase(1).first()) {
-            is Resource.Success -> true
-            is Resource.Error -> false
-        }
+        val actualResource = getNoteDetailUseCase(1).first()
 
-        assertEquals("Resource should be success", true, isSuccess)
+        assertEquals(
+            "Resource should be success",
+            Resource.Success(note),
+            actualResource
+        )
     }
 
     @Test
     fun `Get note detail should return fail`() = runBlocking {
         noteRepositoryFake.setResourceType(ResourceType.ERROR)
 
-        val isSuccess = when (getNoteDetailUseCase(1).first()) {
-            is Resource.Success -> true
-            is Resource.Error -> false
-        }
+        val actualResource = getNoteDetailUseCase(1).first()
 
-        assertEquals("Resource should be fail", false, isSuccess)
+        assertEquals(
+            "Resource should be fail",
+            Resource.Error<Note>(),
+            actualResource
+        )
     }
 }
