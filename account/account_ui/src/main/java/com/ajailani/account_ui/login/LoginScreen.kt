9594efdde966e5d0ce2b6f7c83d33bb964646864
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-07-18 17:55:46 +0700

    Update keyboard type for password text field

--- account/account_ui/src/main/java/com/ajailani/account_ui/login/LoginScreen.kt
+++ account/account_ui/src/main/java/com/ajailani/account_ui/login/LoginScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import com.tonotes.core.R
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Lock
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -77,7 +79,10 @@ fun LoginScreen() {
                             )
                         }
                     },
-                    singleLine = true
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    )
                 )
                 Spacer(modifier = Modifier.height(30.dp))
                 Button(
