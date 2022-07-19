4774d09c825511e3ce9738a60b8b853c43abc971
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-07-19 08:32:20 +0700

    Update HomeViewModelTest

--- note/note_ui/src/test/java/com/tonotes/note_ui/viewmodel/HomeViewModelTest.kt
+++ note/note_ui/src/test/java/com/tonotes/note_ui/viewmodel/HomeViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.tonotes.note_ui.viewmodel
 
+import com.tonotes.core.domain.use_case.GetAccessTokenUseCase
 import com.tonotes.core.util.Resource
 import com.tonotes.core.util.UIState
 import com.tonotes.note_domain.model.Note
@@ -30,13 +31,16 @@ class HomeViewModelTest {
     @Mock
     private lateinit var getNotesUseCase: GetNotesUseCase
 
+    @Mock
+    private lateinit var getAccessTokenUseCase: GetAccessTokenUseCase
+
     private lateinit var homeViewModel: HomeViewModel
 
     private lateinit var onEvent: (HomeEvent) -> Unit
 
     @Before
     fun setUp() {
-        homeViewModel = HomeViewModel(getNotesUseCase)
+        homeViewModel = HomeViewModel(getNotesUseCase, getAccessTokenUseCase)
         onEvent = homeViewModel::onEvent
         onEvent(HomeEvent.OnSearchQueryChanged(""))
     }
