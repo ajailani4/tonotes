3852ae3bb49d80b8d66ca8f8c571349b004db4e6
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-07-20 08:16:42 +0700

    Add Register screen UI

---
+++ account/account_ui/src/main/java/com/tonotes/account_ui/register/RegisterScreen.kt
@@ -0,0 +1,151 @@
+package com.tonotes.account_ui.register
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import com.tonotes.account_ui.login.LoginEvent
+import com.tonotes.core.R
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen() {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        ) {
+            IconButton(
+                modifier = Modifier.padding(
+                    top = 8.dp,
+                    start = 4.dp
+                ),
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back icon"
+                )
+            }
+            Column(
+                modifier = Modifier.padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.register),
+                    style = MaterialTheme.typography.headlineLarge
+                )
+                Spacer(modifier = Modifier.height(60.dp))
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = "",
+                    onValueChange = {},
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Person,
+                            contentDescription = "Username icon"
+                        )
+                    },
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.username))
+                    },
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = "",
+                    onValueChange = {},
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Badge,
+                            contentDescription = "Name icon"
+                        )
+                    },
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.name))
+                    },
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = "",
+                    onValueChange = {},
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Lock,
+                            contentDescription = "Password icon"
+                        )
+                    },
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.password))
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = {}) {
+                            Icon(
+                                imageVector = if (/*passwordVisibility*/false) {
+                                    Icons.Outlined.VisibilityOff
+                                } else {
+                                    Icons.Outlined.Visibility
+                                },
+                                contentDescription = "Password visibility icon"
+                            )
+                        }
+                    },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password
+                    ),
+                    visualTransformation = if (/*passwordVisibility*/false) VisualTransformation.None else PasswordVisualTransformation()
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {}
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.register),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+                Spacer(modifier = Modifier.height(15.dp))
+                ClickableText(
+                    text = buildAnnotatedString {
+                        append(stringResource(id = R.string.have_account))
+                        append(" ")
+
+                        withStyle(
+                            style = SpanStyle(
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        ) {
+                            append(stringResource(id = R.string.login_here))
+                        }
+                    },
+                    style = MaterialTheme.typography.bodyLarge,
+                    onClick = {}
+                )
+            }
+        }
+    }
+}
