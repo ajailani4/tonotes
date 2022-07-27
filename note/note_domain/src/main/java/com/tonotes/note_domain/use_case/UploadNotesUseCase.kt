2f4e0d106e499b6375b8f925208cf64bbebf2163
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-07-27 10:13:42 +0700

    Update UploadNotesUseCase

--- note/note_domain/src/main/java/com/tonotes/note_domain/use_case/UploadNotesUseCase.kt
+++ note/note_domain/src/main/java/com/tonotes/note_domain/use_case/UploadNotesUseCase.kt
@@ -1,10 +1,6 @@
 package com.tonotes.note_domain.use_case
 
-import androidx.work.*
-import com.tonotes.core.util.Constants.WorkerName
-import com.tonotes.note_data.repository.NoteRepository
-import com.tonotes.note_data.worker.UploadNotesWorker
-import java.util.concurrent.TimeUnit
+import com.tonotes.note_domain.repository.NoteRepository
 import javax.inject.Inject
 
 class UploadNotesUseCase @Inject constructor(
