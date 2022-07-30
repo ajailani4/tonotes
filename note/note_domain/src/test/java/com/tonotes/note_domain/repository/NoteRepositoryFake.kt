68f32de2fac28800279f9bbf566efbd1cd2fc2a4
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-07-30 13:52:23 +0700

    Update NoteRepositoryFake

--- note/note_domain/src/test/java/com/tonotes/note_domain/repository/NoteRepositoryFake.kt
+++ note/note_domain/src/test/java/com/tonotes/note_domain/repository/NoteRepositoryFake.kt
@@ -3,7 +3,6 @@ package com.tonotes.note_domain.repository
 import com.tonotes.core.util.Resource
 import com.tonotes.core.util.convertToDate
 import com.tonotes.note_domain.model.Note
-import com.tonotes.note_domain.util.ResourceType
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
@@ -17,21 +16,13 @@ class NoteRepositoryFake : NoteRepository {
         )
     )
 
-    private lateinit var resourceType: ResourceType
+    override fun getNotes(searchQuery: String) = flowOf(notes)
 
-    override fun getNotes(searchQuery: String): Flow<Resource<List<Note>>> =
-        when (resourceType) {
-            ResourceType.SUCCESS -> flowOf(Resource.Success(notes))
+    override fun syncNotes(): Flow<Resource<String>> {
+        TODO("Not yet implemented")
+    }
 
-            ResourceType.ERROR -> flowOf(Resource.Error(null))
-        }
-
-    override fun getNoteDetail(id: Int): Flow<Resource<Note>> =
-        when (resourceType) {
-            ResourceType.SUCCESS -> flowOf(Resource.Success(notes.find { it.id == id }))
-
-            ResourceType.ERROR -> flowOf(Resource.Error(null))
-        }
+    override fun getNoteDetail(id: Int) = flowOf(notes.find { it.id == id }!!)
 
     override suspend fun insertNote(note: Note) {
         TODO("Not yet implemented")
@@ -55,9 +46,5 @@ class NoteRepositoryFake : NoteRepository {
 
     override fun getSelectedBackupType(): Flow<Int> {
         TODO("Not yet implemented")
-    }
-
-    fun setResourceType(type: ResourceType) {
-        resourceType = type
     }
 }
