14110e12da758b7e1b82a54184e6429af7db814c
Ahmad Jailani <ahmadjailani604@gmail.com>
2022-08-04 07:21:56 +0700

    Add function to create notification channel

--- app/src/main/java/com/tonotes/app/App.kt
+++ app/src/main/java/com/tonotes/app/App.kt
@@ -1,8 +1,13 @@
 package com.tonotes.app
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.tonotes.core.util.Constants
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -12,8 +17,28 @@ class App : Application(), Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
     override fun getWorkManagerConfiguration() =
         Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val backupStatusChannel = NotificationChannel(
+                Constants.NotificationChannel.BACKUP_STATUS_ID,
+                Constants.NotificationChannel.BACKUP_STATUS_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = Constants.NotificationChannel.BACKUP_STATUS_DESCRIPTION
+            }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(backupStatusChannel)
+        }
+    }
 }
